39e9706360aed742b14f1c3b9702cb653b93a286
fxnxtic <thedinalt@gmail.com>
2025-12-16 18:43:35 +0400

    fix: resolve few bugs in setup funcs

--- app/bot/setup.py
+++ app/bot/setup.py
@@ -12,11 +12,12 @@ from raito.utils.configuration import RaitoConfiguration
 from redis.asyncio import Redis
 
 import app._const as c
+from app.bot.middlewares import apply_middleware_to_all, HandleUpdatesMiddleware
+from app.bot.utils.roles import CustomRoleManager, UserServiceRoleProvider
+from app.services.users import UserService
 
 
-async def setup_fsm_storage(
-    redis: Redis | None = None, **kwargs
-) -> RedisStorage | MemoryStorage:
+async def setup_fsm_storage(redis: Redis | None = None) -> RedisStorage | MemoryStorage:
     storage = RedisStorage(redis) if redis else MemoryStorage()
     return storage
 
@@ -24,17 +25,24 @@ async def setup_fsm_storage(
 async def setup_dispatcher(storage: BaseStorage, **kwargs) -> Dispatcher:
     dp = Dispatcher(storage=storage, **kwargs)
 
+    apply_middleware_to_all(dp, HandleUpdatesMiddleware())
+
     return dp
 
 
-async def setup_bot(bot_token: str, bot_api_url: str, defaults: dict | None = None, **kwargs) -> Bot:
+async def setup_bot(
+    bot_token: str,
+    bot_api_url: str | None = None,
+    defaults: dict | None = None,
+    **kwargs,
+) -> Bot:
     session = None
-    if bot_api_url != c.BOT_API_URL:
+    if isinstance(bot_api_url, str) and bot_api_url != c.BOT_API_URL:
         session = AiohttpSession(api=TelegramAPIServer.from_base(bot_api_url))
 
     default = DefaultBotProperties(**defaults) if defaults else None
 
-    bot = Bot(token=bot_token, session=session, default=default**kwargs)
+    bot = Bot(token=bot_token, session=session, default=default, **kwargs)
 
     return bot
 
@@ -42,18 +50,19 @@ async def setup_bot(bot_token: str, bot_api_url: str, defaults: dict | None = No
 async def setup_raito(
     dispatcher: Dispatcher,
     routers_dir: str | Path,
+    user_service: UserService,
     *,
     developers: list[int] | None = None,
-    locales: list[str] | None = None,
     production: bool = True,
-    configuration: RaitoConfiguration | None = None,
     storage: BaseStorage | None = None,
 ) -> Raito:
+    configuration = RaitoConfiguration(
+        role_manager=CustomRoleManager(provider=UserServiceRoleProvider(user_service))
+    )
     raito = Raito(
         dispatcher=dispatcher,
         routers_dir=routers_dir,
         developers=developers,
-        locales=locales,
         production=production,
         configuration=configuration,
         storage=storage,
