005fbf964f263e3ef65e84f022abd5428b4ffb43
fxnxtic <thedinalt@gmail.com>
2025-12-16 18:43:51 +0400

    feat: add message texts

---
+++ app/bot/content/text.py
@@ -0,0 +1,6 @@
+instructions_cmd = """<b>🔼 Current instructions.</b>\n\n<i>You can load new instructions from <code>.txt</code> file.</i>"""
+instructions_empty_cmd = """<b>No instructions now.</b>\n\n<i>You can load new instructions from <code>.txt</code> file.</i>"""
+instructions_updated = """<b>Instructions updated.</b>"""
+
+context_cmd = """<b>🔼 Current chat context.</b>"""
+context_empty_cmd = """<b>Current chat context is empty.</b>"""
